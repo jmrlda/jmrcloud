277b66b73b99b5e94c7c158e9604859c2d10a21d
guirruta <yotlanga@gmail.com>
2020-02-26 10:51:59 +0200

    #2 - criar subdiretorio view no directorio rota e mover o ficheiro html

--- servicos/primobile/api/rota/index.js
+++ servicos/primobile/api/rota/index.js
@@ -1,9 +1,9 @@
 const express        = require('express');
 const server         = express();
-
+const path            = require('path');
 
 server.get('/', ( req, res ) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + path.normalize('/view/index.html'));
 });
 
 module.exports = server;
