0fb8abd0bd0e91cb4a0e455c40f024f65ac35a94
dercio guirruta <dercioguirruta44@gmail.com>
2020-04-02 07:49:28 +0200

    #8 - Criar template e adicionar a paginas parcialmente e suas respectivas rotas

--- rota/index.js
+++ rota/index.js
@@ -3,6 +3,40 @@ const server         = express();
 const path            = require('path');
 
 server.get('/', ( req, res ) => {
-    res.render('paginas/index');
+    res.render('paginas/index', {
+        title: "JMRcloud Dashboard",
+        isActive: "dashboard"
+    });
 });
+
+server.get('/clientes', ( req, res ) => {
+    res.render('paginas/cliente/cliente', {
+        title: "Clientes",
+        isActive: "clientes"
+    });
+});
+
+server.get('/clientes/novo', ( req, res ) => {
+    res.render('paginas/cliente/cliente_crud', {
+        title: "Clientes",
+        isActive: "clientes"
+    });
+});
+
+server.get('/servicos', ( req, res ) => {
+    res.render('paginas/servicos', {
+        title: "Servicos",
+        isActive: "servicos"
+    });
+});
+
+
+server.get('/aplicacao', ( req, res ) => {
+    res.render('paginas/aplicacao', {
+        title: "apliacao",
+        isActive: "aplicacao"
+    });
+});
+
+
 module.exports = server;
